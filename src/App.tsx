47cacf99ccb702b6d847141b3c7bd67fddbb5a0c
unknown <ramovic225@gmail.com>
2024-10-30 22:39:14 +0100

    categories functionality done

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,21 @@
-import './App.css'
-import Categories from './components/Categories'
-import Header from './components/Header'
+import "./App.css";
+import Categories from "./components/Categories";
+import Header from "./components/Header";
 
 function App() {
-
   return (
-    <>
+    <div className="flex flex-col">
       <Header />
-      <Categories />
-    </>
-  )
+      <div className="grid grid-cols-[auto, 1fr] flex-grow overflow-auto">
+        <div>Sidebar</div>
+        <div className="overflow-hidden px-6 py-4">
+          <div className="sticky top-0 z-20">
+            <Categories />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default App
+export default App;
--- src/components/Categories.tsx
+++ src/components/Categories.tsx
@@ -1,5 +1,7 @@
-import { useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import Button from "./Button";
+import ArrowBackIosNewOutlinedIcon from "@mui/icons-material/ArrowBackIosNewOutlined";
+import ArrowForwardIosOutlinedIcon from "@mui/icons-material/ArrowForwardIosOutlined";
 
 const Categories = () => {
   const categoryData = [
@@ -14,15 +16,41 @@ const Categories = () => {
     "Coding",
   ];
 
+  const [translate, setTranslate] = useState(0);
   const [selectedCategory, setSelectedCategory] = useState(categoryData[0]);
+  const [isLeftArrowVisible, setIsLeftArrowVisible] = useState(false);
+  const [isRightArrowVisible, setIsRightArrowVisible] = useState(false);
+  const containerRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    if (containerRef.current === null) return;
+    const observer = new ResizeObserver((entries) => {
+      const container = entries[0]?.target;
+      if (container === null) return;
+
+      setIsLeftArrowVisible(translate > 0);
+      setIsRightArrowVisible(
+        translate + container.clientWidth < container.scrollWidth
+      );
+    });
+
+    observer.observe(containerRef.current);
+
+    return () => {
+      observer.disconnect();
+    };
+  }, [categoryData, translate]);
 
   return (
-    <div className="relative overflow-x-hidden px-10">
-      <div className="flex items-center whitespace-nowrap gap-2 w-[max-content]">
+    <div className="relative overflow-x-hidden" ref={containerRef}>
+      <div
+        className="flex whitespace-nowrap gap-2 w-fit transition-transform"
+        style={{ transform: `translateX(-${translate}px)` }}
+      >
         {categoryData.map((category) => {
           return (
             <Button
-              className="px-3 py-1 rounded-lg"
+              className="px-3 py-1 rounded-lg whitespace-nowrap"
               variant={selectedCategory === category ? "dark" : "default"}
               key={category}
               onClick={() => setSelectedCategory(category)}
@@ -32,6 +60,48 @@ const Categories = () => {
           );
         })}
       </div>
+      {isLeftArrowVisible && (
+        <div className="w-24 h-full absolute left-0 top-1/2 -translate-y-1/2 bg-gradient-to-r from-white from-50% to-transparent">
+          <Button
+            className="aspect-square h-full w-auto"
+            variant="ghost"
+            size="icon"
+            type="button"
+            onClick={() =>
+              setTranslate((translate) => {
+                const newTranslate = translate - 200;
+                if (newTranslate <= 0) return 0;
+                return newTranslate;
+              })
+            }
+          >
+            <ArrowBackIosNewOutlinedIcon />
+          </Button>
+        </div>
+      )}
+      {isRightArrowVisible && (
+        <div className="w-24 h-full flex justify-end absolute right-0 top-1/2 -translate-y-1/2 bg-gradient-to-l from-white from-50% to-transparent">
+          <Button
+            className="aspect-square h-full w-auto"
+            variant="ghost"
+            size="icon"
+            type="button"
+            onClick={() => {
+              setTranslate((translate) => {
+                if (containerRef.current === null) return translate;
+                const newTranslate = translate + 200;
+                const containerWidth = containerRef.current.scrollWidth;
+                const width = containerRef.current.clientWidth;
+                if (newTranslate + width >= containerWidth)
+                  return containerWidth - width;
+                return newTranslate;
+              });
+            }}
+          >
+            <ArrowForwardIosOutlinedIcon />
+          </Button>
+        </div>
+      )}
     </div>
   );
 };
