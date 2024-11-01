23927a2ad5112c08fa2d4ec755e9feea9387afac
unknown <ramovic225@gmail.com>
2024-11-01 23:10:51 +0100

    Large sidebar item added

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -3,7 +3,7 @@ import AccountCircleOutlinedIcon from "@mui/icons-material/AccountCircleOutlined
 import { ReactNode } from "react";
 import { twMerge } from "tailwind-merge";
 import { buttonStyles } from "./Button";
-import SmartDisplayOutlinedIcon from '@mui/icons-material/SmartDisplayOutlined';
+import SmartDisplayOutlinedIcon from "@mui/icons-material/SmartDisplayOutlined";
 import SubscriptionsOutlinedIcon from "@mui/icons-material/SubscriptionsOutlined";
 
 export const Sidebar = () => {
@@ -31,26 +31,40 @@ export const Sidebar = () => {
   ];
 
   return (
-    <div className="sticky top-0 overflow-y-auto flex flex-col">
-      {sideBarItems.map((item) => (
-        <SmallSideBarItem
-          key={item.name}
-          icon={item.icon}
-          name={item.name}
-          url={item.url}
-        />
-      ))}
-    </div>
+    <>
+      <div className="lg:hidden sticky top-0 overflow-y-auto flex flex-col ml-1">
+        {sideBarItems.map((item) => (
+          <SmallSidebarItem
+            key={item.name}
+            icon={item.icon}
+            name={item.name}
+            url={item.url}
+          />
+        ))}
+      </div>
+      <div className="lg:sticky absolute top-0 overflow-y-auto flex flex-col gap-2">
+        <LargeSidebarContainer>
+          <LargeSidebarItem />
+        </LargeSidebarContainer>
+      </div>
+    </>
   );
 };
 
-type SmallSideBarItemProps = {
+type SmallSidebarItemProps = {
   icon: ReactNode;
   name: string;
   url: string;
 };
 
-const SmallSideBarItem = ({ icon, name, url }: SmallSideBarItemProps) => {
+type LargeSidebarItemProps = {
+  icon: ReactNode;
+  name: string;
+  url: string;
+  isActive?: boolean;
+};
+
+const SmallSidebarItem = ({ icon, name, url }: SmallSidebarItemProps) => {
   return (
     <a
       href={url}
@@ -63,4 +77,28 @@ const SmallSideBarItem = ({ icon, name, url }: SmallSideBarItemProps) => {
       <p className="text-sm">{name}</p>
     </a>
   );
+};
+
+const LargeSidebarItem = ({
+  icon,
+  name,
+  url,
+  isActive = false,
+}: LargeSidebarItemProps) => {
+  return (
+    <a
+      href={url}
+      className={twMerge(
+        buttonStyles({ variant: "ghost" }),
+        "w-full flex items-center gap-3 rounded-lg"
+      )}
+    >
+      <div className="w-6 h-6">{icon}</div>
+      <p className="whitespace-nowrap overflow-hidden text-ellipsis">{name}</p>
+    </a>
+  );
+};
+
+const LargeSidebarContainer = () => {
+  return <div></div>;
 };
