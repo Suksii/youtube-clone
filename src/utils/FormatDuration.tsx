5c35c94df8ec5f26a47588b7bca8a674aa7c731c
unknown <ramovic225@gmail.com>
2024-11-08 23:02:37 +0100

    FormatDuration added

---
+++ src/utils/FormatDuration.tsx
@@ -0,0 +1,14 @@
+type DurationType = number;
+
+export const FormatDuration = (duration: DurationType): string => {
+  const hours = Math.floor(duration / 3600);
+  const minutes = Math.floor((duration % 3600) / 60);
+  const seconds = duration % 60;
+
+  const hoursFormatted = hours > 0 ? `${hours}:` : "";
+  const minutesFormatted =
+    hours > 0 ? String(minutes).padStart(2, "0") : minutes.toString();
+  const secondsFormatted = String(seconds).padStart(2, "0");
+
+  return `${hoursFormatted}${minutesFormatted}:${secondsFormatted}`;
+};
