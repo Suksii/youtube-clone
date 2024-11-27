1d82a64e4eccc9ed9657fb1b39f6325eac4b82c7
unknown <ramovic225@gmail.com>
2024-11-27 13:17:20 +0100

    related videos skeleton created

---
+++ src/components/RelatedVideos.tsx
@@ -0,0 +1,56 @@
+import MoreVertIcon from "@mui/icons-material/MoreVert";
+import Button from "./Button";
+
+const RelatedVideos = () => {
+  const relatedVideos = [
+    {
+      videoUrl: "",
+      videoTitle: "YouTube video title",
+      channelName: "Channel Name",
+    },
+    {
+      videoUrl: "",
+      videoTitle: "YouTube video title",
+      channelName: "Channel Name",
+    },
+    {
+      videoUrl: "",
+      videoTitle: "YouTube video title",
+      channelName: "Channel Name",
+    },
+  ];
+
+  return (
+    <div className="flex flex-col gap-2">
+      {relatedVideos.map((relatedVideo, index) => (
+        <div className="flex gap-2 relative" key={index}>
+          <div
+            style={{ flex: 2 }}
+            className="rounded-lg bg-gray-400 aspect-video"
+          >
+            video
+          </div>
+          <div style={{ flex: 3 }} className="flex flex-col">
+            <h2 className="font-semibold line-clamp-2">
+              {relatedVideo.videoTitle}
+            </h2>
+            <p className="text-sm text-secondary-text">
+              {relatedVideo.channelName}
+            </p>
+            <p className="text-sm text-secondary-text">
+              {relatedVideo.channelName}
+            </p>
+          </div>
+          <Button
+            className="absolute top-0 right-0 rounded-full w-9 h-9 flex items-center justify-center"
+            variant="ghost"
+          >
+            <MoreVertIcon fontSize="small" />
+          </Button>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default RelatedVideos;
--- src/components/pages/VideoPage.tsx
+++ src/components/pages/VideoPage.tsx
@@ -1,5 +1,5 @@
-import MoreVertIcon from "@mui/icons-material/MoreVert";
-import Button from "../Button";
+import RelatedVideos from "../RelatedVideos";
+
 const VideoPage = () => {
   // const videos = [
   //     {
@@ -16,24 +16,9 @@ const VideoPage = () => {
           style={{ flex: 3 }}
         ></iframe>
         <div
-          className="bg-gray-200 rounded-xl p-4 flex-col"
           style={{ flex: 1 }}
         >
-          <div className="flex gap-2 relative">
-            <div style={{ flex: 2 }} className="rounded-xl bg-gray-400 aspect-video">
-              video
-            </div>
-            <div style={{ flex: 3 }} className="flex flex-col">
-              <h2 className="font-semibold line-clamp-2">
-                YouTube video title
-              </h2>
-              <p className="text-sm text-secondary-text">Channel Name</p>
-              <p className="text-sm text-secondary-text">Channel Name</p>
-            </div>
-            <Button className="absolute top-0 right-0 rounded-full w-8 h-8 flex items-center justify-center" variant="ghost">
-              <MoreVertIcon fontSize="small"/>
-            </Button>
-          </div>
+          <RelatedVideos />
         </div>
       </div>
     </div>
